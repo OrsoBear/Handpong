18123b3f9e5373a7b361b75d8bd7fc6469fa0df2
OrsoBear <nakai@live.it>
2023-06-23 16:44:35 +0200

    Aggiunto overlay base

--- sketch.js
+++ sketch.js
@@ -1,13 +1,8 @@
 let capture
 let detector
 
-
-function preload() {
-}
-
 async function setup() {
 
-	createCanvas(640, 480, WEBGL)
 
 	capture = createCapture(VIDEO)
 	capture.size(640, 480)
@@ -17,18 +12,6 @@ async function setup() {
 	detector = await createDetector()
 	console.log("Modello caricato.")
 
-}
-
-async function draw() {
-    translate (-width/2, -height/2)
-
-	background(255)
-    
-	// Disegna la webcam sullo stage, a specchio
-	push()
-	scale(-1, 1)
-	image(capture, -640, 0)
-	pop()
 }
 
 async function createDetector() {
